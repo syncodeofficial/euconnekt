c1b0df6d6f5a0a8f89eb02262d43d9fbfdf1d92e
Syncode <syncodeoficial@gmail.com>
2026-02-11 13:34:33 +0000

    Add metadata object for site title, description, and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,15 @@ import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import "./globals.css";
 
+export const metadata = {
+  title: "Euconnekt Services",
+  description: "Descrição do site",
+  icons: {
+    icon: "/icon.png",
+  },
+};
+
+
 export default function RootLayout({
   children,
 }: {
